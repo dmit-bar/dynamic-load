bb999b0231f10564c40823a46664cb52dec5a874
Барыкин Дмитрий <dmit.bar716@gmail.com>
2023-02-02 19:33:21 +0300

    no splitting

--- src/pages/first-page/first-page.tsx
+++ src/pages/first-page/first-page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import * as _ from "../../lib/lodash";
 
 const FirstPage = () => {
   const navigate = useNavigate();
 
   return (
     <div className="page">
-      <h2>{_.capitalize("first page")}</h2>
+      <h2>First page</h2>
       <button onClick={() => navigate("/second")}>To second page</button>
     </div>
   );
